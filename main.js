4ed806c0195aaab1c4b868ffdd4c43e1e8c754dc
Nyxiie-Nek <ribeiro.cruz.victor@escola.pr.gov.br>
2025-08-07 07:44:12 -0300

    Para de assistir muckbang Yasmen

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-const burros = segundoB
+
+const palpite = document.querySelector('.palpite');
+const ultimoResultado = document.querySelector('.ultimoResultado');
+const baixoOuAlto = document.querySelector('.baixoOuAlto');
+const envioPalpite = document.querySelector('.envioPalpite');
+const campoPalpite = document.querySelector('.campoPalpite');
+
+
+
+function verificarPalpite () {
+    const palpiteUsuario = Number(campoPalpite.value);
+    if (contagemPalpites === 1) {
+        palpites.textContent = "Palpites anterioes: ";
+    }
+
+    palpites.textContent += palpiteUsuario + " ";
+
+    if (palpiteUsuario === numeroAleratorio)
+        ultimoResultado.textContent = "Acerto Mizeravi";
+
+}
