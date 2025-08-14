55aa321fa2b70805dd9d43f04ef781952a2763f2
Nyxiie-Nek <ribeiro.cruz.victor@escola.pr.gov.br>
2025-08-14 07:42:44 -0300

    WOOOOOOOOOOOOOOO YEAH BABY THAT'S WHAT WE'VE BE WAITING FOR

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const campoPalpite = document.querySelector('.campoPalpite');
 let contagemPalpites = 1;
 let botaoReinicar;
 
-
 function verificarPalpite () {
     const palpiteUsuario = Number(campoPalpite.value);
     if (contagemPalpites === 1) {
@@ -17,8 +16,21 @@ function verificarPalpite () {
     palpites.textContent += palpiteUsuario + " ";
 
     if (palpiteUsuario === numeroAleratorio)
-        ultimoResultado.textContent = "Acertô mizeravi!!!";
+        ultimoResultado.textContent = "Acertô mizeravi >:3";
         ultimoResultado.style.backgroundColor = "green";
     baixoOuAlto.textContent = "";
     finalizarJogo();
+}    else if (conatgemPalpites === 10) {
+    ultimoResultado.textContext = "Não foi dessa vez >~<";
+    baixoOuAlto.textcontent = "";
+    finalizarJogo();
+} else {
+    ultimoResultado.textContent = "Errado";
+    ultimoResultado.style.backgroundColor = "red";
+    if (palpiteUsuario < numeroAleatorio) {
+        baixoOuAlto.textContent = "O seu número é menor";
+    } else if (palpiteUsuario > numeroAleatorio) {
+        baixoOuAlto.textContent = "O seu número é maior";
+    }
 }
+        
